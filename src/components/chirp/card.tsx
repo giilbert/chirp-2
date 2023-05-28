12bf3911c5fd42364111d6f81d1cef54a132ccfd
Gilbert Zhang <gilbertzhang1208@gmail.com>
2023-05-27 20:20:20 -0400

    make date formatting a bit better

--- src/components/chirp/card.tsx
+++ src/components/chirp/card.tsx
@@ -4,12 +4,26 @@ import moment from "moment";
 const betterFormatDate = (date: Date) => {
   const dateMoment = moment(date);
   const now = moment();
+  const diffHours = now.diff(dateMoment, "hours");
+  const diffDays = now.diff(dateMoment, "days");
 
-  // if the current date is not the same, return a full date
-  if (dateMoment.date() !== now.date()) return dateMoment.format();
+  // if the time is less than 6 hours ago, return a relative time
+  if (diffHours < 6) return dateMoment.fromNow();
+
+  // if it was more than a month ago, return a date
+  if (diffDays > 30) {
+    // if not in the same year, return a year
+    if (now.year() !== dateMoment.year())
+      return dateMoment.format("MMM D, YYYY");
+    // otherwise return a month and day
+    return dateMoment.format("MMM D");
+  }
+
+  // if the date is not the same, return a month and a day
+  if (now.date() !== dateMoment.date()) return dateMoment.format("MMM D");
 
   // otherwise only return a time
-  return dateMoment.format("HH:MM:SS A");
+  return dateMoment.format("H:MM A");
 };
 
 export const ChirpCard: React.FC<{ chirp: Chirp & { author: Profile } }> = ({
@@ -25,8 +39,10 @@ export const ChirpCard: React.FC<{ chirp: Chirp & { author: Profile } }> = ({
           <p>{chirp.author.displayName}</p>
           <p className="text-muted-foreground">@{chirp.author.username}</p>
 
+          <p className="text-muted-foreground">·</p>
+
           <p className="text-muted-foreground">
-            &#11049; {betterFormatDate(chirp.createdAt)}
+            {betterFormatDate(chirp.createdAt)}
           </p>
         </div>
 
