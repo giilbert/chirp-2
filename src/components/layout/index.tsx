be14f0cacbb26c511628cd313b7df9cc6bb4af8c
Gilbert Zhang <gilbertzhang1208@gmail.com>
2023-05-29 20:43:34 -0400

    loading bar!

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -23,9 +23,15 @@ export const Layout: React.FC<
     <>
       <div className="flex justify-center">
         <div className="flex w-screen lg:grid lg:max-w-[150rem] lg:grid-cols-4">
-          <div className="min-w-[54px] lg:col-span-1" />
+          <div className="min-w-[54px] lg:col-span-1">
+            {/* the progress bar has some issues, this is here to cover those issues */}
+            <div className="sticky left-0 top-0 z-10 h-2 w-full border-r bg-background backdrop-blur-sm" />
+          </div>
           <div className="z-10 col-span-2 w-full">{children}</div>
-          <div className="hidden h-full w-[32rem] md:block lg:w-full"></div>
+          <div className="hidden h-full w-[32rem] md:block lg:w-full">
+            {/* see comment above */}
+            <div className="sticky left-0 top-0 z-10 h-2 w-full border-l bg-background backdrop-blur-sm" />
+          </div>
         </div>
       </div>
 
