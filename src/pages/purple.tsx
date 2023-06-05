4da6b449279042f474e38c66fdc2e855daa705f4
Gilbert Zhang <gilbertzhang1208@gmail.com>
2023-06-05 19:16:35 -0400

    animate purple page sections

--- src/pages/purple.tsx
+++ src/pages/purple.tsx
@@ -1,13 +1,25 @@
 import { Layout } from "@/components/layout";
 import type { NextPage } from "next";
 import styles from "@/styles/purple.module.css";
-import { motion } from "framer-motion";
+import { type Variants, motion } from "framer-motion";
 import {
   BadgeCheckIcon,
   MicIcon,
   SlidersHorizontalIcon,
   TicketIcon,
 } from "lucide-react";
+
+const sectionVariants: Variants = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+    },
+  },
+};
 
 const PurplePage: NextPage = () => {
   return (
@@ -114,38 +126,55 @@ const PurplePage: NextPage = () => {
 
         <hr className="mt-16" />
 
-        <div className="border-b p-8">
-          <BadgeCheckIcon className="mb-4 h-24 w-24 text-purple-500" />
-          <h1 className="text-2xl font-bold md:text-4xl">You get a badge.</h1>
-          <p>
-            This badge will show up everywhere - on your profile, on your
-            chirps, making you better than everyone else. Who doesn&apos;t want
-            a badge?
-          </p>
-        </div>
+        <motion.div
+          variants={{
+            hidden: {
+              opacity: 0,
+            },
+            show: {
+              opacity: 1,
+              transition: {
+                delayChildren: 5,
+                staggerChildren: 1,
+              },
+            },
+          }}
+          initial="hidden"
+          animate="show"
+        >
+          <motion.div className="border-b p-8" variants={sectionVariants}>
+            <BadgeCheckIcon className="mb-4 h-24 w-24 text-purple-500" />
+            <h1 className="text-2xl font-bold md:text-4xl">You get a badge.</h1>
+            <p>
+              This badge will show up everywhere - on your profile, on your
+              chirps, making you better than everyone else. Who doesn&apos;t
+              want a badge?
+            </p>
+          </motion.div>
 
-        <div className="border-b p-8">
-          <MicIcon className="mb-4 h-24 w-24 text-purple-500" />
-          <h1 className="text-2xl font-bold md:text-4xl">
-            Protection from punishments.
-          </h1>
-          <p>
-            With Purple, you will not be able to be silenced on this platform.
-            Basically you&apos; buying youre freedom to speak (This isn&apos;t
-            the US).
-          </p>
-        </div>
+          <motion.div className="border-b p-8" variants={sectionVariants}>
+            <MicIcon className="mb-4 h-24 w-24 text-purple-500" />
+            <h1 className="text-2xl font-bold md:text-4xl">
+              Protection from punishments.
+            </h1>
+            <p>
+              With Purple, you will not be able to be silenced on this platform.
+              Basically you&apos; buying youre freedom to speak (This isn&apos;t
+              the US).
+            </p>
+          </motion.div>
 
-        <div className="border-b p-8">
-          <SlidersHorizontalIcon className="mb-4 h-24 w-24 text-purple-500" />
-          <h1 className="text-2xl font-bold md:text-4xl">
-            More favored in the algorithm.
-          </h1>
-          <p>
-            Make yourself more discoverable, by boosting your rank in the Chirp
-            algorithm. More people will be able to find you with Purple.
-          </p>
-        </div>
+          <motion.div className="border-b p-8" variants={sectionVariants}>
+            <SlidersHorizontalIcon className="mb-4 h-24 w-24 text-purple-500" />
+            <h1 className="text-2xl font-bold md:text-4xl">
+              More favored in the algorithm.
+            </h1>
+            <p>
+              Make yourself more discoverable, by boosting your rank in the
+              Chirp algorithm. More people will be able to find you with Purple.
+            </p>
+          </motion.div>
+        </motion.div>
       </motion.div>
     </Layout>
   );
